cce27282a741e90b3f63c8fcf1e9faaff11dc506
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-08-21 10:03:44 +0200

    edit bugs

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,23 +1,29 @@
 import xmltodict
 import json
+import yaml
 import os
 from glob import glob
 
-from utils.utils import read_xml
+from utils.utils import *
 
 class PREPROCESS():
 
-    def __init__(self, path='../data/articles_raw_data/'):
+    def __init__(self, path='../data/articles_raw_data/', config_path='config.yaml'):
         self.path = path
 
         self.file_paths = glob(os.path.join(self.path, '*.xml'))
-        self.time_periods = sorted([i.split('/')[-1].split('.')[1] for i in self.file_names])
+        self.time_periods = sorted([i.split('/')[-1].split('.')[1] for i in self.file_paths])
 
         self.periods = len(self.time_periods)
-        self.articles = {}
-
-    def 
+        self.articles = [get_articles(self.file_paths[i]) for i in range(self.periods)]
+        self.sentences = [get_sentences(self.articles[i]) for i in range(self.periods)]
+        
+        with open(config_path, "rb") as f:
+            self.configs = yaml.load(f, Loader=yaml.FullLoader)
     
+    
+        if self.configs['save_sentences']:
+            save_sentences(self.sentences, self.time_periods)
 
 
 
