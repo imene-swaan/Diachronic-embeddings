82954dde7b0f6fbb8886012c8fe70d61053fadae
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-10-06 21:58:19 +0200

    exceptions and documentation

--- src/embeddings/roberta.py
+++ src/embeddings/roberta.py
@@ -1,10 +1,10 @@
-import os.path
+import os
 import logging
 import tqdm
 from typing import Union, List
 from pathlib import Path
 import math
-from transformers import RobertaTokenizer, RobertaForMaskedLM, DataCollatorForLanguageModeling, get_linear_schedule_with_warmup, logging as lg, pipeline
+from transformers import RobertaTokenizer, RobertaForMaskedLM, DataCollatorForLanguageModeling, get_linear_schedule_with_warmup, logging as lg
 import torch
 from torch.utils.data import DataLoader, Dataset
 import torch.optim as optim
@@ -18,7 +18,6 @@ from src.utils.utils import train_test_split
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
 
 
 class CustomDataset(Dataset):
@@ -50,8 +49,25 @@ class CustomDataset(Dataset):
         return len(self.tokenized_data.input_ids)
 
     def __getitem__(self, idx):
-        return {"input_ids": self.tokenized_data.input_ids[idx], "labels": self.tokenized_data.input_ids[idx]}
+        """
+        Retrieves the item at the specified index.
 
+        Parameters:
+            idx (int): Index of the item to retrieve.
+
+        Returns:
+            dict: Dictionary containing the input_ids, attention_mask, and labels.
+        """
+        # Get the tokenized inputs at the specified index
+        input_ids = self.tokenized_data.input_ids[idx]
+        attention_mask = self.tokenized_data.attention_mask[idx]
+
+        # Return a dictionary containing input_ids, attention_mask, and labels (if applicable)
+        return {
+            'input_ids': input_ids,
+            'attention_mask': attention_mask
+            # Add 'labels': labels if you have labels for your data
+        }
 
 class RobertaTrainer:
     """
@@ -66,12 +82,21 @@ class RobertaTrainer:
         train(data, output_dir: Union[str, Path] = None)
             This method is used to train the model.
     """
-    def __init__(self, model_name="roberta-base", max_length=128, mlm_probability=0.15, batch_size=4, learning_rate=1e-5, epochs=3, warmup_steps=500, split_ratio=0.8):
-        # ... (same as before)
-        self.split_ratio = split_ratio
+    def __init__(self, model_name="roberta-base", max_length=128, mlm_probability=0.15, batch_size=4, learning_rate=1e-5, epochs=3, warmup_steps=500, split_ratio=0.8, truncation=True, padding="max_length"):
+        
+        
         self.tokenizer = RobertaTokenizer.from_pretrained(model_name)
         self.model = RobertaForMaskedLM.from_pretrained(model_name)
-        self.data_collator = DataCollatorForLanguageModeling(tokenizer=self.tokenizer, mlm=True, mlm_probability=mlm_probability)
+
+        self.data_collator = DataCollatorForLanguageModeling(
+            tokenizer=self.tokenizer, 
+            mlm=True, 
+            mlm_probability=mlm_probability
+            )
+
+        self.split_ratio = split_ratio
+        self.truncation = truncation
+        self.padding = padding
         self.max_length = max_length
         self.batch_size = batch_size
         self.learning_rate = learning_rate
@@ -79,26 +104,63 @@ class RobertaTrainer:
         self.warmup_steps = warmup_steps
         self.accelerator = Accelerator()
 
-    def prepare_dataset(self, data):
-        dataset = CustomDataset(data, self.tokenizer, max_length=self.max_length)
-        train_loader = DataLoader(dataset, batch_size=self.batch_size, shuffle=True, collate_fn=self.data_collator)
-        return train_loader
+    def prepare_dataset(self, data: List[str]):
+        dataset = CustomDataset(
+            data, 
+            self.tokenizer, 
+            max_length=self.max_length,
+            truncation=self.truncation,
+            padding=self.padding
+            )
+        
+        train_loader = DataLoader(
+            dataset, 
+            batch_size=self.batch_size, 
+            shuffle=True, 
+            collate_fn=self.data_collator
+            )
+        
+        return train_loader, dataset
 
     def train(
             self, 
-            data,
+            data: List[str],
             output_dir: Union[str, Path] = None
             ):
-        train_data, test_data = train_test_split(data, test_size=1 - self.split_ratio, random_state=42)
-        train_loader = self.prepare_dataset(train_data)
-        test_loader = self.prepare_dataset(test_data)
         
-        optimizer = optim.AdamW(self.model.parameters(), lr=self.learning_rate)
+        train_data, test_data = train_test_split(
+            data, 
+            test_ratio=1 - self.split_ratio, 
+            random_seed=42
+            )
+        
+        train_loader, _ = self.prepare_dataset(train_data)
+        test_loader, _ = self.prepare_dataset(test_data)
+        
+        optimizer = optim.AdamW(
+            self.model.parameters(), 
+            lr=self.learning_rate
+            )
 
-        model, optimizer, train_loader, test_loader = self.accelerator.prepare(self.model, optimizer, train_loader, test_loader)
-        scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=self.warmup_steps, num_training_steps=len(train_loader) * self.epochs)
+        model, optimizer, train_loader, test_loader = self.accelerator.prepare(
+            self.model, 
+            optimizer, 
+            train_loader, 
+            test_loader
+            )
+        
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer, 
+            num_warmup_steps=self.warmup_steps, 
+            num_training_steps=len(train_loader) * self.epochs
+            )
 
-        progress_bar = tqdm.tqdm(range(len(train_loader) * self.epochs), desc="Training", dynamic_ncols=True)
+        progress_bar = tqdm.tqdm(
+            range(len(train_loader) * self.epochs), 
+            desc="Training", 
+            dynamic_ncols=True
+            )
+        
         for epoch in range(self.epochs):
             self.model.train()
 
@@ -142,7 +204,7 @@ class RobertaTrainer:
 
 
 
-class VectorEmbeddings:
+class WordEmbeddings:
     """
     This class is used to infer vector embeddings from a sentence.
 
@@ -226,6 +288,19 @@ class VectorEmbeddings:
 
 
 class MaskedWordInference:
+    """
+    This class is used to infer vector embeddings from a sentence.
+
+    Methods
+    -------
+        __init__(pretrained_model_path:Union[str, Path] = None)
+            The constructor for the VectorEmbeddings class.
+        _roberta_case_preparation()
+            This method is used to prepare the Roberta model for the inference.
+        infer_vector(doc:str, main_word:str)
+            This method is used to infer the vector embeddings of a word from a sentence.
+    """
+
     def __init__(
             self,
             pretrained_model_path:Union[str, Path] = None,
@@ -247,7 +322,7 @@ class MaskedWordInference:
     
     def _roberta_case_preparation(self) -> None:
         """
-        This method is used to prepare the BERT model for the inference.
+        This method is used to prepare the Roberta model for the inference.
         """
         model_path = self.model_path if self.model_path is not None else 'roberta-base'
         self.roberta_tokenizer = RobertaTokenizer.from_pretrained(model_path)
@@ -265,20 +340,41 @@ class MaskedWordInference:
             sentence: str
             ):
         
+        """
+        This method is used to infer the vector embeddings of a word from a sentence.
+        Args:
+            word: Word to mask
+            sentence: Sentence to mask the word in
+            
+        Returns: 
+            mask_token_embedding: torch.Tensor
+        """
+        
         if not self.vocab:
             raise ValueError(
                 f'The Embedding model {self.model.__class__.__name__} has not been initialized'
             )
         
+        if len(sentence.split()) > 128:
+            raise ValueError(
+                f'The sentence: "{sentence}" is too long. The maximum length is 128 tokens'
+            )
+        
         masked_sentence = sentence.replace(word, self.roberta_tokenizer.mask_token)
         tokenized_input = self.roberta_tokenizer(masked_sentence, return_tensors='pt')
-        mask_token_index = torch.where(tokenized_input["input_ids"] == self.roberta_tokenizer.mask_token_id)[1]
 
-        with torch.no_grad():
-            outputs = self.model(**tokenized_input)
-            last_hidden_states = outputs.last_hidden_state  # Get the last hidden states of the tokens
-            mask_token_embedding = last_hidden_states[:, mask_token_index, :]
-        return mask_token_embedding
+        try: 
+            mask_token_index = torch.where(tokenized_input["input_ids"] == self.roberta_tokenizer.mask_token_id)[1]
+            with torch.no_grad():
+                outputs = self.model(**tokenized_input)
+                last_hidden_states = outputs.last_hidden_state  # Get the last hidden states of the tokens
+                mask_token_embedding = last_hidden_states[:, mask_token_index, :]
+            return mask_token_embedding
+    
+        except ValueError:
+            raise ValueError(
+                f'The word: "{word}" does not exist in the list of tokens: {tokenized_input} from {sentence}'
+            )
     
 
     def get_top_k_words(
@@ -287,6 +383,17 @@ class MaskedWordInference:
             sentence: str,
             k: int = 10
             ):
+        """
+        This method is used to infer the vector embeddings of a word from a sentence.
+        Args:
+            word: Word to mask
+            sentence: Sentence to mask the word in
+            k: Number of top words to return
+
+        Returns:
+            top_k_words: list
+            filled_sentences: list
+        """
         if not self.vocab:
             raise ValueError(
                 f'The Embedding model {self.model.__class__.__name__} has not been initialized'
@@ -294,22 +401,31 @@ class MaskedWordInference:
         
 
         masked_sentence = sentence.replace(word, self.roberta_tokenizer.mask_token)
-        tokenized_input = self.roberta_tokenizer(masked_sentence, return_tensors='pt')
-        mask_token_index = torch.where(tokenized_input["input_ids"] == self.roberta_tokenizer.mask_token_id)[1]
+        tokenized_input = self.roberta_tokenizer(masked_sentence, return_tensors='pt', max_length=128)
 
-        with torch.no_grad():
-            logits = self.model(**tokenized_input).logits
-            mask_token_logits = logits[0, mask_token_index, :]
-            top_k_tokens = torch.topk(mask_token_logits, k, dim=1).indices[0].tolist()
+        try:
+            mask_token_index = torch.where(tokenized_input["input_ids"] == self.roberta_tokenizer.mask_token_id)[1]
 
-            top_k_words = []
-            filled_sentences = []
-            for token in top_k_tokens:
-                w = self.roberta_tokenizer.decode([token])
-                top_k_words.append(w)
-                filled_sentences.append(masked_sentence.replace(self.roberta_tokenizer.mask_token, w))
-            
-            return top_k_words, filled_sentences
+            with torch.no_grad():
+                logits = self.model(**tokenized_input).logits
+                mask_token_logits = logits[0, mask_token_index, :]
+                top_k_tokens = torch.topk(mask_token_logits, k, dim=1).indices[0].tolist()
+
+                top_k_words = []
+                filled_sentences = []
+                for token in top_k_tokens:
+                    w = self.roberta_tokenizer.decode([token])
+                    top_k_words.append(w)
+                    filled_sentences.append(masked_sentence.replace(self.roberta_tokenizer.mask_token, w))
+                
+                return top_k_words, filled_sentences
         
+        except ValueError:
+            raise ValueError(
+                f'The word: "{word}" does not exist in the list of tokens: {tokenized_input} from {sentence}'
+            )
+        
+
+
 
 
