3e6f65690b4b4201eaa84bab05145a7dade8ea0e
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-10-20 18:17:48 +0200

    tiral1

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ import os
 
 
 def main(output_dir, data_path, periods, **kwargs):
-    print('*'*10, 'Loading data', '*'*10, '\n')
     corpora = {}
     target_word =kwargs['target_word'][0]
-
     xml_tag = kwargs['xml_tag']
     word2vec_paths = []
+
+
     for period in periods:
+        print(f'Loading data from {period} ...', '\n')
         # loading the data
         path = data_path.format(period)
         corpora[period] = Loader.from_xml(
@@ -28,6 +29,7 @@ def main(output_dir, data_path, periods, **kwargs):
                 ) # Loader.from_txt(path).forward()
 
         # preprocessing
+        print(f'Preprocessing data from {period} ...', '\n')
         corpora[period] = list(map(lambda x: 
                                     PREPROCESS().forward(
                                        x, 
@@ -37,21 +39,28 @@ def main(output_dir, data_path, periods, **kwargs):
                                 )
                             )
         # training the models
+        print(f'Training Roberta from {period} ...', '\n')
         roberta_path = f'{output_dir}/MLM_roberta_{period}'
         trainor = RobertaTrainer(**kwargs['mlm_options'])
         trainor.train(data=corpora[period], output_dir= roberta_path)
 
-        word2vec_path = f'{output_dir}/word2vec_{period}'
+        print(f'Training Word2Vec from {period} ...', '\n')
+        word2vec_path = f'{output_dir}/word2vec_{period}.model'
         word2vec_paths.append(word2vec_path)
         trainor = Word2VecTrainer()
         trainor.train(data=corpora[period], output_dir=word2vec_path)
     
 
     # aligning word2vec
+    print(f'Aligning Word2Vec models ...', '\n')
+
     aligned_word2vec_dir = f'{output_dir}/word2vec_aligned'
-    align = Word2VecAlign(model_paths= word2vec_paths).align_models(reference_index=-1, output_dir=aligned_word2vec_dir, method="procrustes")
+    if not os.path.exists(aligned_word2vec_dir):
+        os.mkdir(aligned_word2vec_dir)
+    Word2VecAlign(model_paths= word2vec_paths).align_models(reference_index=-1, output_dir=aligned_word2vec_dir, method="procrustes")
 
     # feature extraction
+    print(f'Extracting features ...', '\n')
     graph_inputs = {
         "nodes": [],
         "node_types": [],
@@ -61,9 +70,11 @@ def main(output_dir, data_path, periods, **kwargs):
         "edge_types": []
     }
     for i, period in enumerate(periods):
+        print(f'Extracting features from {period} ...', '\n')
         word2vec = Word2VecInference(f'{output_dir}/word2vec_aligned/word2vec_{period}_aligned.model')
         roberta = RobertaInference(f'{output_dir}/MLM_roberta_{period}')
 
+        print(f'Extracting context words ...', '\n')
         context_words = word2vec.get_top_k_words(
             positive=[target_word],
             negative= None,
@@ -71,7 +82,10 @@ def main(output_dir, data_path, periods, **kwargs):
             )
         
         context_nodes = list(set(context_words))
-    
+        print('Length of context nodes: ', len(context_nodes), '\n')
+
+
+        print(f'Extracting similar words ...', '\n')
         similar_words = []
         for i, doc in enumerate(corpora[period]):
             top_k_words = roberta.get_top_k_words(
@@ -82,8 +96,9 @@ def main(output_dir, data_path, periods, **kwargs):
             similar_words.extend(top_k_words)
 
         similar_nodes = list(set(similar_words))
-        
+        print('Length of similar nodes: ', len(similar_nodes), '\n')
 
+        print(f'Creating graph inputs ...', '\n')
 
         node_types = [] # 1 for context, 0 for similar
         nodes = []
@@ -95,7 +110,12 @@ def main(output_dir, data_path, periods, **kwargs):
         graph_inputs['nodes'].append(nodes)
         graph_inputs['node_types'].append(node_types)
 
+        print('Length of nodes: ', len(nodes), '\n')
+        print('Length of node types: ', len(node_types), '\n')
+        print('Example: ', nodes[:10], '\n', node_types[:10], '\n')
 
+
+        print(f'Node features ...', '\n')
         word_embeddings = {node: [] for node in nodes}
         for i, doc in enumerate(corpora[period]):
             for node in nodes:
@@ -104,14 +124,10 @@ def main(output_dir, data_path, periods, **kwargs):
                     word_embeddings[node].append(embedding)
         
         node_features = [np.mean(v) for _,v in word_embeddings.items()]
+        print('Length of node features: ', len(node_features), '\n')
         graph_inputs['node_features'].append(node_features)
         
-
             
-
-
-    
-    
     return graph_inputs
              
         
@@ -121,7 +137,7 @@ def main(output_dir, data_path, periods, **kwargs):
 
 
 if __name__ == "__main__":
-    output_dir = 'output'
+    output_dir = 'trial1'
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
     periods = [
@@ -140,7 +156,7 @@ if __name__ == "__main__":
         # 2010,
         # 2012,
         # 2015,
-        # 2017
+        2017
     ]
 
     xml_tag = 'fulltext'
@@ -159,7 +175,7 @@ if __name__ == "__main__":
         "max_length": 128,
         "mlm_probability": 0.15, 
         "batch_size": 32, 
-        "epochs": 30, 
+        "epochs": 3, 
         "split_ratio": 0.8, 
         "learning_rate": 5e-5,
         "truncation": True, 
@@ -181,7 +197,7 @@ if __name__ == "__main__":
         periods, 
         xml_tag = 'fulltext',
         target_word = target_word,
-        max_documents = 10000,
+        max_documents = 100,
         shuffle = True,
         preprocessing_options = preprocessing_options,
         mlm_options = mlm_options,
