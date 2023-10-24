ed197b5cbbc0457a0c2e21e9406abf608da2d47d
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-10-24 20:15:38 +0200

    forward update

--- src/data/data_preprocessing.py
+++ src/data/data_preprocessing.py
@@ -2,51 +2,66 @@ import re
 
 
 
-# ------------------- cleantxt --------------------
-def cleantxt(text, **kwargs):
-    
-    newtext = re.sub('\n', ' ', text) # Remove ordinary linebreaks (there shouldn't be, so this might be redundant)
-
-    if kwargs.get('remove_punctuation', True):
-        newtext = re.sub(r'[^a-zA-Z0-9\s\.]', '', str(newtext)) # Remove anything that is not a space, a letter, a dot, or a number
-    
-    if kwargs.get('remove_numbers', True):
-        newtext = re.sub(r'[0-9]', '', str(newtext)) # Remove numbers
-    
-    if kwargs.get('lowercase', True):
-        newtext = str(newtext).lower() # Lowercase
-    
-    if kwargs.get('lemmatize', True):
-        from nltk.stem import WordNetLemmatizer
-        from nltk.corpus import wordnet 
-
-        lemmatizer = WordNetLemmatizer()
-        newtext = ' '.join(list(map(lambda x: lemmatizer.lemmatize(x, wordnet.NOUN), newtext.split())))
-    
-    if kwargs.get('remove_stopwords', True):
-        from nltk.corpus import stopwords
-        stop_words = set(stopwords.words('english'))
-        newtext = ' '.join(list(filter(lambda x: x not in stop_words, newtext.split())))
-
-    return newtext
-
 
 class PREPROCESS():
     def __init__(self):
+        """
+        This class is used to preprocess text data.
+        """
         pass
 
     def forward(self,
                 text,
-                **kwargs):
-    
-        return cleantxt(text, **kwargs)
-        
+                remove_punctuation: bool = True,
+                remove_numbers: bool = True,
+                lowercase: bool = True,
+                lemmatize: bool = True,
+                remove_stopwords: bool = True,            
+        ):
 
-    
-        
-    
+        """
+        This function preprocesses text data.
 
+        Args:
+            text (str): Text to be preprocessed.
+            remove_punctuation (bool): Whether to remove punctuation. Defaults to True.
+            remove_numbers (bool): Whether to remove numbers. Defaults to True.
+            lowercase (bool): Whether to lowercase. Defaults to True.
+            lemmatize (bool): Whether to lemmatize. Defaults to True.
+            remove_stopwords (bool): Whether to remove stopwords. Defaults to True.
+        
+        Returns:
+            newtext (str): Preprocessed text.
+        """
+    
+        newtext = re.sub('\n', ' ', text) # Remove ordinary linebreaks (there shouldn't be, so this might be redundant)
+
+        if remove_punctuation:
+            newtext = re.sub(r'[^a-zA-Z0-9\s\.]', '', str(newtext)) # Remove anything that is not a space, a letter, a dot, or a number
+        
+        if remove_numbers:
+            newtext = re.sub(r'[0-9]', '', str(newtext)) # Remove numbers
+        
+        if lowercase:
+            newtext = str(newtext).lower() # Lowercase
+        
+        if lemmatize:
+            from nltk.stem import WordNetLemmatizer
+            from nltk.corpus import wordnet 
+
+            lemmatizer = WordNetLemmatizer()
+            newtext = ' '.join(list(map(lambda x: lemmatizer.lemmatize(x, wordnet.NOUN), newtext.split())))
+        
+        if remove_stopwords:
+            from nltk.corpus import stopwords
+            stop_words = set(stopwords.words('english'))
+            newtext = ' '.join(list(filter(lambda x: x not in stop_words, newtext.split())))
+
+        return newtext
+        
 
 if __name__ == '__main__':
-    text = 'Hello! yes is a stopword. Needing to test \n for lemmatization ALSO!'
-    print(cleantxt(text, remove_stopwords=True, lemmatize=True, lowercase=True, remove_punctuation=True))
+    # Test
+    text = 'This is a test. 1234'
+    print('Original text: ', text)
+    print('Preprocessed text: ', PREPROCESS().forward(text, remove_punctuation=True, remove_numbers=True, lowercase=True, lemmatize=True, remove_stopwords=True))
