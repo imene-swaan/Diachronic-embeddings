b49e816e064270a030fa7b6fe9bbeee77adf3505
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-10-27 20:51:07 +0200

    constructing the graph

---
+++ src/graphs/temporal_graph.py
@@ -0,0 +1,21 @@
+from src.feature_extraction.roberta import RobertaInference
+from src.feature_extraction.bert import BertInference
+from src.feature_extraction.word2vec import Word2VecInference
+
+
+class Nodes:
+    pass
+
+class Edges:
+    pass
+
+class Graph:
+    pass
+
+class TemporalGraph:
+    pass
+
+
+
+if __name__ == '__main__':
+    pass
