ce13dad569af8f6bfb1a50714daefdf1f1ea34b9
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-11-01 14:12:18 +0100

    idea

--- src/graphs/temporal_graph.py
+++ src/graphs/temporal_graph.py
@@ -5,6 +5,27 @@ from pydantic import BaseModel, validator, Field
 from typing import List, Optional, Union
 
 
+# nodes:
+# level 1:
+    # - context nodes: words that are used in the same context as the target word. Extracted from word2vec.
+    # - similar nodes: words that are similar to the target word. Extracted from the MLM by checking the k most likely words to replace the masked target word in each sentence.
+    # - target node: the target word.
+
+# level 2:
+    # - context nodes: context nodes of words in level 1
+    # - similar nodes: similar nodes of words in level 1
+
+# level 3:
+    # - context nodes: context nodes of words in level 2
+    # - similar nodes: similar nodes of words in level 2
+
+# args:
+    # - target word: the word to get the nodes for
+    # - dataset: the sentences to get the nodes from
+    # - k: the number of similar nodes to get for each occurrence of the target word
+    # - c: the number of context nodes to get for the target word
+    # - level: the level of the graph to get
+    # - model_paths: the path to the word2vec model and the MLM model
 
 
 
