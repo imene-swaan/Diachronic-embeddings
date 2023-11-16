e035daf319e9617c9b8533a33e5c26dcfa9e9afa
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-11-16 21:25:28 +0100

    added the temporal Graph

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from semantics.data.data_loader import Loader
 from semantics.data.data_preprocessing import PREPROCESS
-from semantics.data.data_loader import split_xml
-from semantics.feature_extraction.roberta import RobertaTrainer, RobertaInference
-from semantics.feature_extraction.word2vec import Word2VecTrainer, Word2VecAlign, Word2VecInference
-from semantics.graphs.temporal_graph import Nodes
-import numpy as np
+from semantics.feature_extraction.roberta import  RobertaInference
+from semantics.feature_extraction.word2vec import Word2VecInference
+from semantics.graphs.temporal_graph import TemporalGraph
 from pathlib import Path
 import os
-
+import numpy as np
 
 
 def main(output_dir, data_path, periods, **kwargs):
@@ -83,13 +81,13 @@ def main(output_dir, data_path, periods, **kwargs):
                                 )
                             )
         #training the models
-        print(f'Training Roberta from {period} ...', '\n')
-        roberta_path = f'{output_dir}/MLM_roberta_{period}'
-        trainor = RobertaTrainer(**kwargs['mlm_options'])
-        trainor.train(
-            data = corpora[period],
-            output_dir = roberta_path
-            )
+        # print(f'Training Roberta from {period} ...', '\n')
+        # roberta_path = f'{output_dir}/MLM_roberta_{period}'
+        # trainor = RobertaTrainer(**kwargs['mlm_options'])
+        # trainor.train(
+        #     data = corpora[period],
+        #     output_dir = roberta_path
+        #     )
 
 
         # print(f'Training Word2Vec from {period} ...', '\n')
@@ -135,85 +133,35 @@ def main(output_dir, data_path, periods, **kwargs):
     #         )
 
 
-    # feature extraction
-    # print(f'Extracting features ...', '\n')
-    # graph_inputs = {
-    #     "nodes": [],
-    #     "node_types": [],
-    #     "node_features": [],
-    #     "edges": [],
-    #     "edge_features": [],
-    #     "edge_types": []
-    # }
+    # Creating the Temporal Graph
+    print(f'Creating the Temporal Graph ...', '\n')
 
-    # for i, period in enumerate(periods):
-    #     print(f'Extracting features from {period} ...', '\n')
-    #     nodes = Nodes()
+    tg = TemporalGraph()
+    inference_options = kwargs['inference_options']
 
-    #     print(f'Extracting context words ...', '\n')
-    #     context_nodes, _ = nodes.get_context_nodes(
-    #         word=target_word,
-    #         model_path=f'{output_dir}/word2vec_aligned/word2vec_{period}_aligned.model',
-    #         k=kwargs['inference_options']['Context_k']
-    #         )
-    #     print('Length of context nodes: ', len(context_nodes), '\n')
-    #     print('Context nodes: ', context_nodes, '\n')
+    for i, period in enumerate(periods):
+        print(f'Creating the Graph of {period} ...', '\n')
 
+        roberta_path = f'{output_dir}/MLM_roberta_{period}'
+        word2vec_path = f'{output_dir}/word2vec_aligned/word2vec_{period}_aligned.model'
 
-        
+        roberta = RobertaInference(pretrained_model_path= roberta_path)
+        word2vec = Word2VecInference(pretrained_model_path= word2vec_path)
 
-    #     print(f'Extracting similar words ...', '\n')
-    #     similar_words = []
-    #     for i, doc in enumerate(corpora[period][:20]):
-    #         top_k_words = nodes.get_similar_nodes(
-    #             word=target_word,
-    #             sentence=doc,
-    #             model_path=f'{output_dir}/MLM_roberta_{period}',
-    #             k=kwargs['inference_options']['MLM_k']
-    #             )
-            
-    #         if len(top_k_words) > 0:
-    #             similar_words.extend(top_k_words) 
+        tg.add_graph(
+            target_word= target_word[0],
+            level = inference_options['level'],
+            k = inference_options['MLM_k'],
+            c = inference_options['Context_k'],
+            dataset= corpora[period],
+            word2vec_model= word2vec,
+            mlm_model= roberta,
+        )
+    
 
-    #     similar_nodes = list(set(similar_words))
-    #     print('Length of similar nodes: ', len(similar_nodes), '\n')
-    #     print('Similar nodes: ', similar_nodes, '\n')
+    return tg
 
-
-
-
-
-        # print(f'Creating graph inputs ...', '\n')
-
-        # node_types = [] # 1 for context, 0 for similar
-        # nodes = []
-        # for node, type in zip(context_nodes + similar_nodes, [1] * len(context_nodes) + [0] * len(similar_nodes)):
-        #     if node not in nodes:
-        #         nodes.append(node)
-        #         node_types.append(type)
-        
-        # graph_inputs['nodes'].append(nodes)
-        # graph_inputs['node_types'].append(node_types)
-
-        # print('Length of nodes: ', len(nodes), '\n')
-        # print('Length of node types: ', len(node_types), '\n')
-        # print('Example: ', nodes[:], '\n', node_types[:], '\n')
-
-
-        # print(f'Node features ...', '\n')
-        # word_embeddings = {node: [] for node in nodes}
-        # for i, doc in enumerate(corpora[period]):
-        #     for node in nodes:
-        #         if node in doc:
-        #             embedding = roberta.get_embedding(word=node, sentence=doc, mask=False)
-        #             word_embeddings[node].append(embedding)
-        
-        # node_features = [np.mean(v) for _,v in word_embeddings.items()]
-        # print('Length of node features: ', len(node_features), '\n')
-        # graph_inputs['node_features'].append(node_features)
-        
-            
-    # return graph_inputs
+    
              
         
         
@@ -222,26 +170,26 @@ def main(output_dir, data_path, periods, **kwargs):
 
 
 if __name__ == "__main__":
-    output_dir = 'trial1'
+    output_dir = 'output'
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
     periods = [
         1980,
-        # 1982,
-        # 1985,
-        # 1987,
-        # 1989,
-        # 1990,
-        # 1992,
-        # 1995,
-        # 2000,
-        # 2001,
-        # 2005,
-        # 2008,
-        # 2010,
-        # 2012,
-        # 2015,
-        # 2017
+        1982,
+        1985,
+        1987,
+        1989,
+        1990,
+        1992,
+        1995,
+        2000,
+        2001,
+        2005,
+        2008,
+        2010,
+        2012,
+        2015,
+        2017
     ]
 
     xml_tag = 'fulltext'
@@ -271,13 +219,14 @@ if __name__ == "__main__":
     inference_options = {
         "MLM_k": 3,
         "Context_k": 30,
+        "level": 3,
         }
 
     target_word = [
             "office"
         ]
     
-    r = main(
+    tg = main(
         output_dir,
         file_path, 
         periods, 
@@ -290,8 +239,18 @@ if __name__ == "__main__":
         inference_options = inference_options
         )
     
-    # with open(f"{output_dir}/results.json", 'w') as f:
-    #     json.dump(r, f, indent=4)
+    i = 0
+    index, node_features, edge_indices, edge_features, labels, label_indices = tg[i]
+
+    print(f"""Example of graph snapshot {periods[i]}: \n
+      Node index shape: {index.shape} \n
+      Node feature shape: {node_features.shape} \n
+      Edge index shape: {edge_indices.shape} \n
+      Edge feature shape: {edge_features.shape} \n 
+      Labels shape: {labels.shape} \n
+      Labels mask shape: {label_indices.shape}
+      """)
+
 
 
 
