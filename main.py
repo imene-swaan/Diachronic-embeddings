0d484edab3707ad44805e2bb2f13d690d79cec43
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-11-19 19:38:46 +0100

    main entry - experiment

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from semantics.data.data_preprocessing import PREPROCESS
 from semantics.feature_extraction.roberta import  RobertaInference
 from semantics.feature_extraction.word2vec import Word2VecInference
 from semantics.graphs.temporal_graph import TemporalGraph
+from semantics.inference.visualize import WordTraffic, visualize_graph
 from pathlib import Path
 import os
 import numpy as np
+import json
 
 
 def main(output_dir, data_path, periods, **kwargs):
@@ -160,6 +162,40 @@ def main(output_dir, data_path, periods, **kwargs):
             word2vec_model= word2vec,
             mlm_model= roberta,
         )
+
+        if tg[i][2].shape[1] != tg[i][3].shape[0]:
+            print('edge_indices and edge_features do not match')
+            print('edge_indices: ', tg[i][2].shape)
+            print('edge_features: ', tg[i][3].shape)
+            print('period: ', period)
+            print('index: ', tg[i][0])
+            print('xs: ', tg[i][1].shape)
+            print('ys: ', tg[i][4].shape)
+            print('y_indices: ', tg[i][5].shape)
+            raise ValueError
+        
+        index, xs, edge_indices, edge_features, ys, y_indices = tg[i]
+
+        if not os.path.exists(f'{output_dir}/inference_{period}'):
+            os.mkdir(f'{output_dir}/inference_{period}')
+
+        with open(f'{output_dir}/inference_{period}/index.json', 'w') as f:
+            json.dump(index, f, indent=4)
+
+        with open(f'{output_dir}/inference_{period}/xs.npy', 'wb') as f:
+            np.save(f, xs)
+        
+        with open(f'{output_dir}/inference_{period}/edge_indices.npy', 'wb') as f:
+            np.save(f, edge_indices)
+
+        with open(f'{output_dir}/inference_{period}/edge_features.npy', 'wb') as f:
+            np.save(f, edge_features)
+
+        with open(f'{output_dir}/inference_{period}/ysnpy', 'wb') as f:
+            np.save(f, ys)
+
+        with open(f'{output_dir}/inference_{period}/y_indices.npy', 'wb') as f:
+            np.save(f, y_indices)
     
 
     return tg
@@ -178,30 +214,30 @@ if __name__ == "__main__":
         os.mkdir(output_dir)
     periods = [
         1980,
-        # 1982,
-        # 1985,
-        # 1987,
-        # 1989,
-        # 1990,
-        # 1992,
-        # 1995,
-        # 2000,
-        # 2001,
-        # 2005,
-        # 2008,
-        # 2010,
-        # 2012,
-        # 2015,
-        # 2017
+        1982,
+        1985,
+        1987,
+        1989,
+        1990,
+        1992,
+        1995,
+        2000,
+        2001,
+        2005,
+        2008,
+        2010,
+        2012,
+        2015,
+        2017
     ]
 
     xml_tag = 'fulltext'
     file_path = "input/xml/TheNewYorkTimes{}.xml"
 
     preprocessing_options = {
-        "remove_stopwords": False, 
+        "remove_stopwords": True, 
         "remove_punctuation": True, 
-        "remove_numbers": False, 
+        "remove_numbers": True, 
         "lowercase": True, 
         "lemmatize": True
         }
@@ -220,13 +256,13 @@ if __name__ == "__main__":
 
     
     inference_options = {
-        "MLM_k": 3,
-        "Context_k": 2,
-        "level": 2,
+        "MLM_k": 5,
+        "Context_k": 5,
+        "level": 3,
         }
 
     target_word = [
-            "office"
+            "abuse"
         ]
     
     tg = main(
@@ -235,46 +271,51 @@ if __name__ == "__main__":
         periods, 
         xml_tag = 'fulltext',
         target_word = target_word,
-        max_documents = 25000,
+        max_documents = 10000,
         shuffle = True,
         preprocessing_options = preprocessing_options,
         mlm_options = mlm_options,
         inference_options = inference_options
         )
     
-    i = 0
-    index, node_features, edge_indices, edge_features, labels, label_indices = tg[i]
 
-    print(f"""Example of graph snapshot {periods[i]}: \n
-      Node index shape: {index.shape} \n
-      Node feature shape: {node_features.shape} \n
-      Edge index shape: {edge_indices.shape} \n
-      Edge feature shape: {edge_features.shape} \n 
-      Labels shape: {labels.shape} \n
-      Labels mask shape: {label_indices.shape}
-      """)
+    index = tg.snapshots
+    xs = tg.xs
+    edge_indices = tg.edge_indices
+    edge_features = tg.edge_features
+    ys = tg.ys
+    y_indices = tg.y_indices
 
 
-    xs = np.array(tg.xs)
-    edge_indices = np.array(tg.edge_indices)
-    edge_features = np.array(tg.edge_features)
-    ys = np.array(tg.ys)
-    y_indices = np.array(tg.y_indices)
-
-    with open(f'{output_dir}/xs.npy', 'wb') as f:
-        np.save(f, xs)
+    with open(f'{output_dir}/index.json', 'w') as f:
+        json.dump(index, f, indent=4)
     
+    with open(f'{output_dir}/xs.npy', 'wb') as f:
+        np.save(f, np.array(xs))
+
     with open(f'{output_dir}/edge_indices.npy', 'wb') as f:
-        np.save(f, edge_indices)
-
+        np.save(f, np.array(edge_indices))
+    
     with open(f'{output_dir}/edge_features.npy', 'wb') as f:
-        np.save(f, edge_features)
-
+        np.save(f, np.array(edge_features))
+    
     with open(f'{output_dir}/ys.npy', 'wb') as f:
-        np.save(f, ys)
-
+        np.save(f, np.array(ys))
+    
     with open(f'{output_dir}/y_indices.npy', 'wb') as f:
-        np.save(f, y_indices)
+        np.save(f, np.array(y_indices))
+    
+
+
+    fig = visualize_graph(
+        graph= (index, xs, edge_indices, edge_features, ys, y_indices) ,
+        title= 'Graph Visualization',
+        node_label_feature= 0,
+        edge_label_feature= 1
+        )
+    
+    fig.savefig(f'{output_dir}/graph_{periods[i]}.png')
+    
 
 
 
