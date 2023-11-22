58c2fa57468cc540c96cd16c65ea2a6fad4cf254
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-11-22 19:32:31 +0100

    added WordGraph component

--- semantics/utils/components.py
+++ semantics/utils/components.py
@@ -1,28 +1,27 @@
-from pydantic import BaseModel, validator, dataclasses
-from typing import Dict, Union
-import numpy as np
-
-from pydantic import BaseModel, validator
-from typing import Dict, Union
+from pydantic import BaseModel, field_validator
+from typing import Dict
 import numpy as np
 
 class WordGraph(BaseModel):
-    index: Dict[str, Union[Dict[str, int], Dict[str, str]]]
+    index: Dict[str, Dict]
     node_features: np.ndarray
     edge_index: np.ndarray
     edge_features: np.ndarray
     labels: np.ndarray
     label_mask: np.ndarray
 
-    @validator('index')
+    class Config:
+        arbitrary_types_allowed = True
+
+    @field_validator('index')
     def check_index(cls, v):
         if not isinstance(v, dict):
             raise ValueError('index must be a dictionary')
         if 'key_to_index' not in v or 'index_to_key' not in v:
-            raise ValueError('index must contain both key_to_index and index_to_key')
+            raise ValueError('index must contain both key_to_index and index_to_key dictionaries')
         return v
     
-    @validator('node_features')
+    @field_validator('node_features')
     def check_node_features(cls, v):
         if not isinstance(v, np.ndarray):
             raise ValueError('node_features must be a numpy.ndarray')
@@ -30,7 +29,7 @@ class WordGraph(BaseModel):
             raise ValueError('node_features must be a 2D array')
         return v
     
-    @validator('edge_index')
+    @field_validator('edge_index')
     def check_edge_index(cls, v):
         if not isinstance(v, np.ndarray):
             raise ValueError('edge_index must be a numpy.ndarray')
@@ -38,7 +37,7 @@ class WordGraph(BaseModel):
             raise ValueError('edge_index must be a 2D array')
         return v
     
-    @validator('edge_features')
+    @field_validator('edge_features')
     def check_edge_features(cls, v):
         if not isinstance(v, np.ndarray):
             raise ValueError('edge_features must be a numpy.ndarray')
@@ -46,7 +45,7 @@ class WordGraph(BaseModel):
             raise ValueError('edge_features must be a 2D array')
         return v
     
-    @validator('labels')
+    @field_validator('labels')
     def check_labels(cls, v):
         if not isinstance(v, np.ndarray):
             raise ValueError('labels must be a numpy.ndarray')
@@ -54,7 +53,7 @@ class WordGraph(BaseModel):
             raise ValueError('labels must be a 1D array')
         return v
 
-    @validator('label_mask')
+    @field_validator('label_mask')
     def check_label_mask(cls, v):
         if not isinstance(v, np.ndarray):
             raise ValueError('label_mask must be a numpy.ndarray')
@@ -65,9 +64,28 @@ class WordGraph(BaseModel):
 
    
     
+if __name__ == '__main__':
+    g = WordGraph(
+        index={
+            'key_to_index': {'a': 0, 'b': 1, 'c': 2},
+            'index_to_key': {0: 'a', 1: 'b', 2: 'c'}
+        },
+        node_features=np.array([[1, 2], [3, 4], [5, 6]]),
+        edge_index=np.array([[0, 1], [1, 2]]),
+        edge_features=np.array([[0.1, 0.2], [0.3, 0.4]]),
+        labels=np.array([0, 1, 2]),
+        label_mask=np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    )
+    
+    # index={
+    #         'key_to_index': {'a': 0, 'b': 1, 'c': 2},
+    #         'index_to_key': {0: 'a', 1: 'b', 2: 'c'}
+    #     }
+    
+    # print(index['key_to_index']['a'])
+    # print(index['index_to_key'][0])
 
-
-
+    print(g.index['index_to_key'][0])
 
 
 
